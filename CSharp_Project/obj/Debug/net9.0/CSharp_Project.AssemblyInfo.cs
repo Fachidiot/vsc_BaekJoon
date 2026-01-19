f69bcc39fc588d61cac88f0bba5ef6bcc3ddde10
Fachidiot <54926396+Fachidiot@users.noreply.github.com>
2026-01-20 00:14:17 +0900

    # csharp_project

--- CSharp_Project/obj/Debug/net9.0/CSharp_Project.AssemblyInfo.cs
+++ CSharp_Project/obj/Debug/net9.0/CSharp_Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharp_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f7d4b4cf823c81ff9df401055ec67071c1b4902")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2cfaab85763826df44ce0fe59e532e9e13de75f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharp_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharp_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
